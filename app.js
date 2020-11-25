1f442ac1f9ef85db5a6d0f45af5e343227773988
yyj-star <jackyyjyang@gmail.com>
2020-11-24 21:33:32 -0500

    added 2 question pages with bootstrap

--- app.js
+++ app.js
@@ -12,8 +12,24 @@ app.use(express.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs({ defaultLayout: false }));
 app.set('view engine', 'handlebars');
 
+const handlebarsInstance = exphbs.create({
+	defaultLayout: 'main',
+	// Specify helpers which are only registered on this instance.
+	helpers: {
+		asJSON: (obj, spacing) => {
+			if (typeof spacing === 'number') return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+
+			return new Handlebars.SafeString(JSON.stringify(obj));
+		}
+	}
+});
+app.engine('handlebars', handlebarsInstance.engine);
+
 configRoutes(app);
 
-app.listen(process.env.PORT, () => {
-    console.log("Running on port: " + String(process.env.PORT))
-});
+// app.listen(process.env.PORT, () => {
+//     console.log("Running on port: " + String(process.env.PORT))
+// });
+app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+  });
