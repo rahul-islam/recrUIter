280d4b0a0a2120c16207f83ab3bcdc4310e00fd3
Frank Besson <32044707+bessonf@users.noreply.github.com>
2020-11-29 10:40:40 -0500

    Update justin.js

--- routes/justin.js
+++ routes/justin.js
@@ -22,4 +22,8 @@ router.get('/Question1', (req, res) => {
 	res.sendFile(path.resolve('static/justin/Question1.html'));
 });
 
+router.get('/Question2', (req, res) => {
+	res.sendFile(path.resolve('static/justin/Question2.html'));
+});
+
 module.exports = router;
